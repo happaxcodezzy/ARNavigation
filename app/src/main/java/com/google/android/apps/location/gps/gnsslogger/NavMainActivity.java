f7f4aa20d499d152f8c1a5d53b25383a0f154a7e
happaxcodezzy <happaxworkzzy@163.com>
2020-08-01 08:39:08 +0800

    original version without review

---
+++ app/src/main/java/com/google/android/apps/location/gps/gnsslogger/NavMainActivity.java
@@ -0,0 +1,18 @@
+package com.google.android.apps.location.gps.gnsslogger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * A program for AR navigation.
+ * @version 1.0 2020-7.31
+ * @author ARNav team
+ */
+public class NavMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_main);
+    }
+}
